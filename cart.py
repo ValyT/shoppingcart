3b277d8661697d4df185e6fe475ae30efade0a41
ValyT <40295158+ValyT@users.noreply.github.com>
2023-08-09 16:47:26 +0300

    Update cart.py
    Added queue to print items on receipt in the order they're added

--- cart.py
+++ cart.py
@@ -1,15 +1,17 @@
 import typing
 
 from . import abc
-
+import queue
 
 class ShoppingCart(abc.ShoppingCart):
     def __init__(self):
         self._items = dict()
+        self.order = queue.Queue()
 
     def add_item(self, product_code: str, quantity: int):
         if product_code not in self._items:
             self._items[product_code] = quantity
+            self.order.put(product_code)
         else:
             q = self._items[product_code]
             self._items[product_code] = q + quantity
@@ -17,12 +19,18 @@ class ShoppingCart(abc.ShoppingCart):
     def print_receipt(self) -> typing.List[str]:
         lines = []
 
-        for item in self._items.items():
-            price = self._get_product_price(item[0]) * item[1]
+        
+        #for item in self._items.items():
+        while not self.order.empty():
+            
+            product_code=self.order.get()
+            amount=self._items[product_code]
+
+            price = self._get_product_price(product_code) * amount
 
             price_string = "€%.2f" % price
 
-            lines.append(item[0] + " - " + str(item[1]) + ' - ' + price_string)
+            lines.append(product_code + " - " + str(amount) + ' - ' + price_string)
 
         return lines
 
